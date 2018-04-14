cf2f3f9b179c8baa11277af8b65ebfdca945ba57
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2018-04-14 11:29:27 -0700

    silence brew warning

--- march.rb
+++ march.rb
@@ -5,7 +5,7 @@ class March < Formula
   sha256 "d01ee74be395b1d9cbf12edce475bb4981584650e38ba54ec4b35b264479854b"
   version '0.2'
 
-  depends_on ruby: '2.1'
+  depends_on 'ruby' => '2.1'
 
   def install
     # Simply copies an executable and its ruby script dependency to /usr/local/bin.
